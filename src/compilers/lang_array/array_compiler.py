bfbb27544197c46ca9dc4b1ac4a454f48267db48
Julius Arndt <juli.a98@live.de>
2025-06-04 00:53:08 +0200

    Begin array compiler

---
+++ src/compilers/lang_array/array_compiler.py
@@ -0,0 +1,9 @@
+from lang_array.array_astAtom import *
+import lang_array.array_ast as plainAst
+from common.wasm import *
+import lang_array.array_tychecker as array_tychecker
+import lang_array.array_transform as array_transform
+from lang_array.array_compilerSupport import *
+from common.compilerSupport import *
+import common.utils as utils
+
