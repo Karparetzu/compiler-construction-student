79ee776f16f95301151f8262561b25ea6e6d74fa
Julius Arndt <juli.a98@live.de>
2025-06-13 19:06:51 +0200

    Finish liveness

--- src/compilers/assembly/liveness.py
+++ src/compilers/assembly/liveness.py
@@ -79,23 +79,19 @@ class InterfGraphBuilder:
         """
         # No instructions: live before variables are the same as at the end
         if bb.last is None:
-            self.before[(bb.index, 0)] = s
-            self.after[(bb.index, 0)] = s
             return s
         
         # Go through instructions in reverse order and get 'before' of i
         afterCurrent = s
         beforeCurrent = s
-        k = len(bb.instrs) - 1
-        for instr in reversed(bb.instrs):
+        for i in reversed(range(0, len(bb.instrs))):
             # Compute L[after] and L[before] for current instruction
             afterCurrent = beforeCurrent
-            beforeCurrent = (afterCurrent - instrDef(instr)).union(instrUse(instr))
+            beforeCurrent = (afterCurrent - instrDef(bb.instrs[i])) | instrUse(bb.instrs[i])
+                
             # Update self.before and self.after
-            self.before[(bb.index, k)] = beforeCurrent
-            self.after[(bb.index, k)] = afterCurrent
-            # Update index of k
-            k -= 1
+            self.before[(bb.index, i)] = beforeCurrent
+            self.after[(bb.index, i)] = afterCurrent
 
         return beforeCurrent
 
@@ -113,22 +109,29 @@ class InterfGraphBuilder:
         verticesIn: dict[int, Tuple[set[tac.ident], bool]] = {}
         for v in vertices:
             verticesIn[v] = (set(), True)
-        
-        # TODO: multiple cycles (idea: mark as dirty if changed in current cycle. Every v is dirty initially. If L[start] hasn't changed => dirty = false. We're finished if all vertices aren't dirty)
-        
-        # For each basic block
-        for v in vertices:
-            # Get successors
-            succs = g.succs(v)
+                
+        # As long as there's at least one vertex which is dirty (had a change during last cycle)
+        while any(dirty == True for (_, dirty) in verticesIn.values()):
+            # For each basic block
+            for v in verticesIn.keys():
+                # Get successors
+                succs = g.succs(v)
 
-            # Compute L[out]
-            lOut: set[tac.ident] = set()
-            for s in succs:
-                lIn = self.before.get((s, 0))
-                if lIn is not None:
-                    lOut.union(lIn)
-            self.liveStart(g.getData(v), lOut)
+                # Compute L[out]
+                lOut: set[tac.ident] = set()
+                for s in succs:
+                    lIn = verticesIn[s][0]
+                    lOut |= lIn
+                
+                # Compute L[start]
+                lStart = self.liveStart(g.getData(v), lOut)
 
+                # If lStart has changed: update and mark as dirty
+                if verticesIn[v][0] != lStart:
+                    verticesIn[v] = (lStart, True)
+                else:
+                    # Else keep previous value and not dirty anymore
+                    verticesIn[v] = (verticesIn[v][0], False)
 
     def __addEdgesForInstr(self, instrId: InstrId, instr: tac.instr, interfG: InterfGraph):
         """
@@ -138,7 +141,11 @@ class InterfGraphBuilder:
         You should implement the algorithm specified on the slide
         "Computing the interference graph" (slide 50) here.
         """
-        raise ValueError('todo')
+        defs = instrDef(instr)
+        after = self.after[instrId] - defs
+        for tgt in after:
+            for src in defs:
+                interfG.addEdge(src, tgt)
 
     def build(self, g: ControlFlowGraph) -> InterfGraph:
         """
@@ -146,11 +153,31 @@ class InterfGraphBuilder:
 
         - Use liveness to fill the sets self.before and self.after.
         - Setup the interference graph as an undirected graph containing all variables
-          defined or used by any instruction of any basic block. Initially, the
+          defined or used by any instruction of any basic block. Initially, theinstr
           graph does not have any edges.
         - Use __addEdgesForInstr to fill the edges of the interference graph.
         """
-        raise ValueError('todo')
+        # 1. Compute liveness for the CFG
+        self.liveness(g)
+        
+        # 2. Setup interference graph (just add union of all defs and uses)
+        # Note: can't use before and after because those don't contain defined but not used variables
+        allVars: set[tac.ident] = set()
+        for bb in g.values:
+            for instr in bb.instrs:
+                allVars |= instrDef(instr)
+                allVars |= instrUse(instr)
+
+        iGraph: InterfGraph = Graph('undirected')
+        for var in allVars:
+            iGraph.addVertex(var, None)
+
+        # 3. Fill edges by going through each instruction in each block
+        for bb in g.values:
+            for i in range(0, len(bb.instrs)):
+                self.__addEdgesForInstr((bb.index, i), bb.instrs[i], iGraph)
+
+        return iGraph
 
 def buildInterfGraph(g: ControlFlowGraph) -> InterfGraph:
     builder = InterfGraphBuilder()
